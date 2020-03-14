1ec66d762938879a714224cbe0dd760ea77fda9b
Achim Schnell <a.schnell@wtschnell.de>
2020-03-14 13:30:41 +0100

    Fix ReSharper-Issues

--- src/FluiTec.AppFx.Options.Tests/ConfigurationExtensionTest.cs
+++ src/FluiTec.AppFx.Options.Tests/ConfigurationExtensionTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using FluiTec.AppFx.Options.Managers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
