c3b617b5fc236a491c72ad434b18a14e6677ce80
Achim Schnell <a.schnell@wtschnell.de>
2020-03-14 13:30:11 +0100

    Add configuration-extension for simplified handling and tests for it

---
+++ src/FluiTec.AppFx.Options.Tests/ConfigurationExtensionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using FluiTec.AppFx.Options.Managers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluiTec.AppFx.Options.Tests
+{
+    [TestClass]
+    public class ConfigurationExtensionTest
+    {
+        [TestMethod]
+        public void TestAutoNamedOptions()
+        {
+            var services = new ServiceCollection();
+            const string stringSetting = "test";
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>($"{nameof(OptionWithDefaultKey)}:{nameof(OptionWithDefaultKey.StringSetting)}", stringSetting),
+                });
+            var config = builder.Build();
+            var manager = new ReportingConfigurationManager(config, s => {});
+            services.Configure<OptionWithDefaultKey>(manager);
+
+            var serviceProvider = services.BuildServiceProvider();
+            Assert.AreEqual(stringSetting, serviceProvider.GetService<IOptions<OptionWithDefaultKey>>().Value.StringSetting);
+        }
+
+        [TestMethod]
+        public void TestManualNamedOptions()
+        {
+            var services = new ServiceCollection();
+            const string stringSetting = "test";
+            const string sectionKey = "MyKey";
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>($"{sectionKey}:{nameof(OptionWithDefaultKey.StringSetting)}", stringSetting),
+                });
+            var config = builder.Build();
+            var manager = new ReportingConfigurationManager(config, s => { });
+            services.Configure<OptionWithDefaultKey>(manager, sectionKey);
+
+            var serviceProvider = services.BuildServiceProvider();
+            Assert.AreEqual(stringSetting, serviceProvider.GetService<IOptions<OptionWithDefaultKey>>().Value.StringSetting);
+        }
+    }
+}
--- src/FluiTec.AppFx.Options/Extensions/ConfigurationExtension.cs
+++ src/FluiTec.AppFx.Options/Extensions/ConfigurationExtension.cs
@@ -1,6 +1,41 @@
-﻿namespace FluiTec.AppFx.Options.Extensions
+﻿using System;
+using FluiTec.AppFx.Options.Managers;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
 {
+    /// <summary>Simple extension to simplify using the ConfigurationManager.</summary>
     public static class ConfigurationExtension
     {
+        /// <summary>Configures the specified manager.</summary>
+        /// <typeparam name="TSettings">The type of the settings.</typeparam>
+        /// <param name="services">The services.</param>
+        /// <param name="manager">The manager.</param>
+        /// <returns>The configured settings.</returns>
+        /// <exception cref="System.ArgumentNullException">services or manager</exception>
+        public static TSettings Configure<TSettings>(this IServiceCollection services, ConfigurationManager manager)
+            where TSettings : class, new()
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (manager == null) throw new ArgumentNullException(nameof(manager));
+
+            return manager.Configure<TSettings>(services);
+        }
+
+        /// <summary>Configures the specified manager.</summary>
+        /// <typeparam name="TSettings">The type of the settings.</typeparam>
+        /// <param name="services">The services.</param>
+        /// <param name="manager">The manager.</param>
+        /// <param name="configurationKey">The configuration key.</param>
+        /// <returns>The configured settings.</returns>
+        /// <exception cref="System.ArgumentNullException">services or manager</exception>
+        public static TSettings Configure<TSettings>(this IServiceCollection services, ConfigurationManager manager, string configurationKey)
+            where TSettings : class, new()
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (manager == null) throw new ArgumentNullException(nameof(manager));
+
+            return manager.Configure<TSettings>(services, configurationKey);
+        }
     }
 }
