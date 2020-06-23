104927e9bf441d33534b01d53275927738263fca
Achim Schnell <a.schnell@wtschnell.de>
2020-06-23 14:30:16 +0200

    Fix reporting collections

--- src/FluiTec.AppFx.Options/Managers/ReportingConfigurationManager.cs
+++ src/FluiTec.AppFx.Options/Managers/ReportingConfigurationManager.cs
@@ -159,14 +159,21 @@ namespace FluiTec.AppFx.Options.Managers
                     var isSecret = p.GetCustomAttributes(true)
                                        .SingleOrDefault(a => a.GetType() == typeof(ConfigurationSecretAttribute)) !=
                                    null;
-                    var value = p.GetValue(settings);
-                    ReportAction(string.Format($"{indent}{PropertyReport}", p.Name,
-                        isSecret ? RedactedValueReplacement : value));
+                    try
+                    {
+                        var value = p.GetValue(settings);
+                        ReportAction(string.Format($"{indent}{PropertyReport}", p.Name,
+                            isSecret ? RedactedValueReplacement : value));
 
-                    if (value == null) continue;
-                    var valueType = value.GetType();
-                    if (!UninspectedTypes.Contains(valueType) && !isSecret)
-                        ReportSettingProperties(value, indentation + 1);
+                        if (value == null) continue;
+                        var valueType = value.GetType();
+                        if (!UninspectedTypes.Contains(valueType) && !isSecret)
+                            ReportSettingProperties(value, indentation + 1);
+                    }
+                    catch (System.Reflection.TargetParameterCountException)
+                    {
+                        // ignore
+                    }
                 }
             }
             else
