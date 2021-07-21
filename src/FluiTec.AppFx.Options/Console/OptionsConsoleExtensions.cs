9840f130a9245da2eda59a01be54889f4c47d819
Achim Schnell <a.schnell@wtschnell.de>
2021-07-21 15:38:33 +0200

    add extension for easy console-configuration

---
+++ src/FluiTec.AppFx.Options/Console/OptionsConsoleExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using FluiTec.AppFx.Console;
+using FluiTec.AppFx.Console.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluiTec.AppFx.Options.Console
+{
+    /// <summary>   The options console extensions. </summary>
+    public static class OptionsConsoleExtensions
+    {
+        /// <summary>   Configure options console module. </summary>
+        /// <param name="services"> The services. </param>
+        public static void ConfigureOptionsConsoleModule(this IServiceCollection services)
+        {
+            ConsoleHost.ConfigureModule(services, provider =>
+            {
+                var conf = provider.GetRequiredService<IConfigurationRoot>();
+                var cp = conf.Providers.Single(p => p is SaveableJsonConfigurationProvider);
+                return new OptionsConsoleModule(cp);
+            });
+        }
+    }
+}
